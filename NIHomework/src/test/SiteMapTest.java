736f621f234f2c72921ec55b39add1da66313c84
Melinda Darvasi <lindarvasi@gmail.com>
2017-04-30 22:31:31 +0200

    update SiteMapTest - add output file operations

--- NIHomework/src/test/SiteMapTest.java
+++ NIHomework/src/test/SiteMapTest.java
@@ -3,7 +3,7 @@ package test;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -24,17 +24,39 @@ import org.openqa.selenium.firefox.FirefoxProfile;
  */
 public class SiteMapTest {
 
+	/**
+	 * 
+	 */
 	private WebDriver driver;
 
+	/**
+	 * 
+	 */
+	private int depth;
+
+	/**
+	 * 
+	 */
+	private String url;
+
+	/**
+	 * 
+	 */
+	private Output out;
+
+	/**
+	 * 
+	 */
 	@Before
 	public void init() {
+		// init Selenium Webdriver
 		File pathBinary = new File(Keys.BROWSER_URL);
 		FirefoxBinary firefoxBinary = new FirefoxBinary(pathBinary);
 		FirefoxProfile firefoxProfile = new FirefoxProfile();
 		driver = new FirefoxDriver(firefoxBinary, firefoxProfile);
 
+		// load config.properties file
 		Properties prop = new Properties();
-
 		try (InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(Keys.CONFIG_FILE)) {
 			prop.load(is);
 
@@ -42,24 +64,69 @@ public class SiteMapTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		// init output file
+		out = new Output(Keys.OUTPUT_FILE);
+
+		// load config params from properties file
+		depth = Integer.parseInt(prop.getProperty(Keys.DEPTH));
+		url = prop.getProperty(Keys.URL);
 	}
 
 	@After
 	public void finish() {
+		// close output file
+		out.close();
+		// close Selenium Webdriver
 		driver.quit();
 	}
 
+	/**
+	 * If depth is 0, then just the first page is loaded, the links of this page
+	 * will be print out to the output file.
+	 * 
+	 */
 	@Test
-	public void collectLinks() {
+	public void siteMapTest() {
+		collectLinks(url, 0);
+	}
 
+	/**
+	 * This method load the page of the param url and collect links from this
+	 * page.
+	 * 
+	 * @param url
+	 * @param level
+	 */
+	public void collectLinks(final String url, final int level) {
+
+		if (level == depth) {
+			// leaf node - print out without size then end the recursion
+			// print out link and linkList size
+			out.write(",,," + level + url + ",,\n");
+			return;
+		}
+
+		// branch node, gotta visit
+
+		driver.get(url);
 		List<WebElement> linkList = driver.findElements(By.tagName("a"));
-		Iterator it = linkList.iterator();
 
-		System.out.println(linkList.size());
-		WebElement e;
-		while (it.hasNext()) {
-			e = (WebElement) it.next();
-			System.out.println(e.getAttribute("outerHTML"));
+		// print out link and linkList size
+		out.write(",,," + level + url + linkList.size() + ",,\n");
+
+		List<String> urlList = new ArrayList<>();
+		// linkList.stream().map(e ->
+		// e.getAttribute("href")).forEach(urlList::add);
+
+		for (WebElement e : linkList) {
+			urlList.add(e.getAttribute("href"));
+		}
+
+		// List<String> urlList2 = linkList.stream().map(e ->
+		// e.getAttribute("href")).collect(Collectors.toList());
+
+		for (String childUrl : urlList) {
+			collectLinks(childUrl, level + 1);
 		}
 	}
 }

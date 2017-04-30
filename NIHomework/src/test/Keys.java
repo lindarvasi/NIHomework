ba184581fbc124b98b72dda6772c6a6163b25bd0
Melinda Darvasi <lindarvasi@gmail.com>
2017-04-30 22:23:19 +0200

    update Keys class file

--- NIHomework/src/test/Keys.java
+++ NIHomework/src/test/Keys.java
@@ -6,12 +6,15 @@ package test;
  *
  */
 public class Keys {
-	
+
 	public static final String BROWSER_URL = "C:\\Program Files\\Mozilla Firefox 3 Beta 5\\firefox.exe";
 
 	public static final String CONFIG_FILE = "config.properties";
 
 	public static final String URL = "url";
 
-	
+	public static final String DEPTH = "depth";
+
+	public static final String OUTPUT_FILE = "out.csv";
+
 }
